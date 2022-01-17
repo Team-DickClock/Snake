4647d8d81d2bd1153b980135e39fe44e3f585654
Arnaud <ducreyarn@gmail.com>
2022-01-17 11:42:32 +0100

    pomme pas ok

--- src/GestionSnake.java
+++ src/GestionSnake.java
@@ -10,7 +10,7 @@ public class GestionSnake {
     int longueur;
     int headX;
     int headY;
-     
+    boolean endGame = false;
 
     public GestionSnake(int height, int width, int initialLength, int headX, int headY) {
         game = new int[height][width];
@@ -37,44 +37,44 @@ public class GestionSnake {
             }
             // Initialisation du snake de début
             for (int i = headX - longueur; i < headX; i++) {
-                game[headY][i] = headX - i;
+                game[headY][i + 1] = headX - i;
             }
         }
     }
 
     public boolean checkValue(int x, int y) {
         // Vérification que les valeurs choisies sont corrects
-        if (y  < 1 || y > game.length - 1) {
+        if (y < 1 || y > game.length - 1) {
             System.out.println("This is not the right value for headY");
             return false;
         }
-        if (x - longueur < 1 || x > game[0].length - 1) {
+        if (x - (longueur - 1) < 1 || x > game[0].length - 1) {
             System.out.println("This is not the right value for headX");
             return false;
         }
         return true;
     }
 
-    public int[] coordPomme(int[][] game){
-        int[] temp = new int[1];
+    public int[] coordPomme(int[][] game) {
+        int[] temp = new int[2];
         Random rand = new Random();
         temp[0] = rand.nextInt(game.length - 2) + 1;
         temp[1] = rand.nextInt(game[0].length - 2) + 1;
         return temp;
     }
 
-    public int[] gestionPomme(int[][]game){
-        int[] temp = new int[1];
+    public int[] gestionPomme(int[][] game) {
+        int[] temp = new int[2];
         boolean pomme = false;
 
-         // Est-ce qu'il reste une pomme sur l'écran
-         for (int i = 0; i < game.length; i++) {
+        // Est-ce qu'il reste une pomme sur l'écran
+        for (int i = 0; i < game.length; i++) {
             for (int j = 0; j < game[0].length; j++) {
                 if (game[i][j] == -1)
                     pomme = true;
             }
-             // Placer une pomme de manière aléatoire
-             do {
+            // Placer une pomme de manière aléatoire
+            do {
                 temp = coordPomme(game);
             } while (game[temp[0]][temp[1]] != 0);
 
@@ -85,7 +85,7 @@ public class GestionSnake {
         return temp;
     }
 
-    public void deplacementSnake(char direction){
+    public void deplacementSnake(char direction) {
         switch (direction) {
             case 'w':
                 this.headY--;
@@ -103,16 +103,17 @@ public class GestionSnake {
                 break;
         }
         // Gestion de la case qui vient
-        int a =this.game[this.headY][this.headX]; 
         if (this.game[this.headY][this.headX] == -1) {
             longueur++;
             this.game[headY][headX] = 0;
         }
-        if (a == -2) {
+        if (this.game[this.headY][this.headX] == -2) {
             System.out.println("YOU LOSE");
+            endGame = true;
         }
         if (this.game[this.headY][this.headX] > 0) {
             System.out.println("YOU LOSE");
+            endGame = true;
         }
 
         // gestion du mouvement du snake
@@ -120,13 +121,13 @@ public class GestionSnake {
             for (int j = 1; j < this.game[0].length - 1; j++) {
                 if (this.game[i][j] > 0) {
                     this.game[i][j] += 1;
-                
+
                 }
             }
         }
 
         this.game[headY][headX] = 1;
-        
+
         for (int i = 1; i < this.game.length - 1; i++) {
             for (int j = 1; j < this.game[0].length - 1; j++) {
                 if (this.game[i][j] > longueur) {
@@ -137,12 +138,14 @@ public class GestionSnake {
     }
 
     public static void main(String[] args) {
-        /* int longueur = 4;
-        int headX = 8;
-        int headY = 1;
-        int[][] game = new int[10][10];
-        Random rand = new Random(); */
-        GestionSnake s = new GestionSnake(10, 10, 4, 8,1);
+        /*
+         * int longueur = 4;
+         * int headX = 8;
+         * int headY = 1;
+         * int[][] game = new int[10][10];
+         * Random rand = new Random();
+         */
+        GestionSnake s = new GestionSnake(10, 10, 4, 4, 1);
         char dir;
         for (int i = 0; i < s.game.length; i++) {
             for (int j = 0; j < s.game[0].length; j++) {
@@ -151,11 +154,11 @@ public class GestionSnake {
             System.out.println();
         }
 
-        while(true){
+        while (!s.endGame) {
             System.out.println("Entrez la prohaine direction:");
             dir = Input.readChar();
-            //s.gestionPomme(s.game);
             s.deplacementSnake(dir);
+            s.game[s.gestionPomme(s.game)[0]][s.gestionPomme(s.game)[1]] = - 1;
 
             for (int i = 0; i < s.game.length; i++) {
                 for (int j = 0; j < s.game[0].length; j++) {
@@ -166,131 +169,130 @@ public class GestionSnake {
         }
 
         // initialisation du tableau avec que des 0
-        /*for (int i = 0; i < game.length; i++) {
-            for (int j = 0; j < game[0].length; j++) {
-                game[i][j] = 0;
-            }
-        }
-        for (int i = 0; i < game.length; i++) {
-            game[i][0] = -2;
-            game[i][game.length - 1] = -2;
-        }
-
-        for (int i = 0; i < game[0].length; i++) {
-            game[0][i] = -2;
-            game[game[0].length - 1][i] = -2;
-        }
-
-        // Initialisation du snake de début
-        
-         for (int i = 1; i < longueur + 1; i++) {
-         game[1][i] = longueur - i + 1;
-         }
-        
-        for (int i = headX - longueur; i <= headX; i++) {
-            game[headY][i] = headX - i+1;
-        }
-
-        // affichage de la situation de départ
-        for (int i = 0; i < game.length; i++) {
-            for (int j = 0; j < game[0].length; j++) {
-                System.out.print(game[i][j] + " ");
-            }
-            System.out.println();
-        }
-        char dir = 'd';
-        boolean pomme = false;
-        int pommeY = 1;
-        int pommeX = 7;
-
-        while (true) {
-
-            pomme = false;
-
-            // lecture de la direction
-            System.out.println("Entrez la prohaine direction:");
-
-            char dirPrev = dir;
-            dir = Input.readChar();
-            // Déplacememnt de la tete
-            if ((dirPrev == 's' && dir == 'w') || (dirPrev == 'w' && dir == 's') || (dirPrev == 'a' && dir == 'd')
-                    || (dirPrev == 'd' && dir == 'a')) {
-                dir = dirPrev;
-            }
-
-            switch (dir) {
-                case 'w':
-                    headY--;
-                    break;
-                case 'a':
-                    headX--;
-                    break;
-                case 's':
-                    headY++;
-                    break;
-                case 'd':
-                    headX++;
-                    break;
-                default:
-                    break;
-            }
-
-            // Gestion de la case qui vient
-            if (game[headY][headX] == -1) {
-                longueur++;
-                game[headY][headX] = 0;
-            }
-            if (game[headY][headX] == -2) {
-                System.out.println("YOU LOSE");
-                break;
-            }
-            if (game[headY][headX] > 0) {
-                System.out.println("YOU LOSE");
-                break;
-            }
-
-            // gestion du mouvement du snake
-            for (int i = 1; i < game.length - 1; i++) {
-                for (int j = 1; j < game[0].length - 1; j++) {
-                    if (game[i][j] > 0) {
-                        game[i][j] += 1;
-                    }
-                }
-            }
-
-            game[headY][headX] = 1;
-
-            for (int i = 1; i < game.length - 1; i++) {
-                for (int j = 1; j < game[0].length - 1; j++) {
-                    if (game[i][j] > longueur) {
-                        game[i][j] = 0;
-                    }
-                }
-            }
-
-            // Gestion des pommes
-            // Est-ce qu'il reste une pomme sur l'écran
-
-            for (int i = 0; i < game.length; i++) {
-                for (int j = 0; j < game[0].length; j++) {
-                    if (game[i][j] == -1)
-                        pomme = true;
-                }
-            }
-            // Placer une pomme de manière aléatoire
-            do {
-                pommeY = rand.nextInt(game.length - 2) + 1;
-                pommeX = rand.nextInt(game[0].length - 2) + 1;
-
-            } while (game[pommeY][pommeX] != 0);
-            if (!pomme) {
-                game[pommeY][pommeX] = -1;
-            }*/
-
-
-
-         
-        }
+        /*
+         * for (int i = 0; i < game.length; i++) {
+         * for (int j = 0; j < game[0].length; j++) {
+         * game[i][j] = 0;
+         * }
+         * }
+         * for (int i = 0; i < game.length; i++) {
+         * game[i][0] = -2;
+         * game[i][game.length - 1] = -2;
+         * }
+         * 
+         * for (int i = 0; i < game[0].length; i++) {
+         * game[0][i] = -2;
+         * game[game[0].length - 1][i] = -2;
+         * }
+         * 
+         * // Initialisation du snake de début
+         * 
+         * for (int i = 1; i < longueur + 1; i++) {
+         * game[1][i] = longueur - i + 1;
+         * }
+         * 
+         * for (int i = headX - longueur; i <= headX; i++) {
+         * game[headY][i] = headX - i+1;
+         * }
+         * 
+         * // affichage de la situation de départ
+         * for (int i = 0; i < game.length; i++) {
+         * for (int j = 0; j < game[0].length; j++) {
+         * System.out.print(game[i][j] + " ");
+         * }
+         * System.out.println();
+         * }
+         * char dir = 'd';
+         * boolean pomme = false;
+         * int pommeY = 1;
+         * int pommeX = 7;
+         * 
+         * while (true) {
+         * 
+         * pomme = false;
+         * 
+         * // lecture de la direction
+         * System.out.println("Entrez la prohaine direction:");
+         * 
+         * char dirPrev = dir;
+         * dir = Input.readChar();
+         * // Déplacememnt de la tete
+         * if ((dirPrev == 's' && dir == 'w') || (dirPrev == 'w' && dir == 's') ||
+         * (dirPrev == 'a' && dir == 'd')
+         * || (dirPrev == 'd' && dir == 'a')) {
+         * dir = dirPrev;
+         * }
+         * 
+         * switch (dir) {
+         * case 'w':
+         * headY--;
+         * break;
+         * case 'a':
+         * headX--;
+         * break;
+         * case 's':
+         * headY++;
+         * break;
+         * case 'd':
+         * headX++;
+         * break;
+         * default:
+         * break;
+         * }
+         * 
+         * // Gestion de la case qui vient
+         * if (game[headY][headX] == -1) {
+         * longueur++;
+         * game[headY][headX] = 0;
+         * }
+         * if (game[headY][headX] == -2) {
+         * System.out.println("YOU LOSE");
+         * break;
+         * }
+         * if (game[headY][headX] > 0) {
+         * System.out.println("YOU LOSE");
+         * break;
+         * }
+         * 
+         * // gestion du mouvement du snake
+         * for (int i = 1; i < game.length - 1; i++) {
+         * for (int j = 1; j < game[0].length - 1; j++) {
+         * if (game[i][j] > 0) {
+         * game[i][j] += 1;
+         * }
+         * }
+         * }
+         * 
+         * game[headY][headX] = 1;
+         * 
+         * for (int i = 1; i < game.length - 1; i++) {
+         * for (int j = 1; j < game[0].length - 1; j++) {
+         * if (game[i][j] > longueur) {
+         * game[i][j] = 0;
+         * }
+         * }
+         * }
+         * 
+         * // Gestion des pommes
+         * // Est-ce qu'il reste une pomme sur l'écran
+         * 
+         * for (int i = 0; i < game.length; i++) {
+         * for (int j = 0; j < game[0].length; j++) {
+         * if (game[i][j] == -1)
+         * pomme = true;
+         * }
+         * }
+         * // Placer une pomme de manière aléatoire
+         * do {
+         * pommeY = rand.nextInt(game.length - 2) + 1;
+         * pommeX = rand.nextInt(game[0].length - 2) + 1;
+         * 
+         * } while (game[pommeY][pommeX] != 0);
+         * if (!pomme) {
+         * game[pommeY][pommeX] = -1;
+         * }
+         */
 
     }
 
+}
