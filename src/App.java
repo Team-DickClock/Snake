a59b9eb5a7df72a9286f27f71d9e2872ed6bdff7
JuliuChe <94694739+JuliuChe@users.noreply.github.com>
2022-01-20 15:29:28 +0100

    jc_1529_dirdebug

--- src/App.java
+++ src/App.java
@@ -9,11 +9,12 @@
  */
 public class App {
     public static void main(String[] args) throws Exception {
+        Game snakeGame = new Game(10,20);
         while (true) {
-            Game snakeGame = new Game(10,20);
             while (snakeGame.isInProgress()) {
                 Interface.drawOnTerminal(snakeGame.getBoard());
-                snakeGame.play(Interface.getDirectionTerminal());
+                snakeGame.play(Interface.getDirectionFunGraphics());
+                //snakeGame.play(Interface.getDirectionTerminal());
                 /**
                  * Pour indice, voilà une façon propre selon moi de faire la direction avec FunGraphics:
                  * snakeGame.play(snakeGame.interfaceGame.getDirectionFunGraphics());
